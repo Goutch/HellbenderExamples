c677341e6b75caa672a798a28bac1c33626784aa
Gabriel Bouchard <33035583+Goutch@users.noreply.github.com>
2025-01-04 16:32:04 -0500

    added ABeautifulGame model

--- Exemples/Model/ModelScene.h
+++ Exemples/Model/ModelScene.h
@@ -5,9 +5,11 @@
 using namespace HBE;
 
 
-class ModelScene : public Scene {
+class ModelScene : public Scene
+{
 public:
-	ModelScene() {
+	ModelScene()
+	{
 		//-------------------RESOURCES CREATION--------------------------------------
 		ShaderInfo frag_info{SHADER_STAGE_FRAGMENT, "/shaders/defaults/PositionUVNormalTextured.frag"};
 		ShaderInfo vert_info{SHADER_STAGE_VERTEX, "/shaders/defaults/PositionUVNormal.vert"};
@@ -38,11 +40,11 @@ public:
 		pipeline_info.vertex_shader = vert;
 
 		pipeline_info.flags = GRAPHIC_PIPELINE_FLAG_FRONT_COUNTER_CLOCKWISE | //gltf primitives are counterclockwise
-		                      GRAPHIC_PIPELINE_FLAG_ALLOW_EMPTY_DESCRIPTOR;
-		GraphicPipeline *model_pipeline_2_sided = Resources::createGraphicPipeline(
-				pipeline_info, "MODEL_PIPELINE_2_SIDED");
+			GRAPHIC_PIPELINE_FLAG_ALLOW_EMPTY_DESCRIPTOR;
+		GraphicPipeline* model_pipeline_2_sided = Resources::createGraphicPipeline(
+			pipeline_info, "MODEL_PIPELINE_2_SIDED");
 		pipeline_info.flags |= GRAPHIC_PIPELINE_FLAG_CULL_BACK;
-		GraphicPipeline *model_pipeline = Resources::createGraphicPipeline(pipeline_info, "MODEL_PIPELINE");
+		GraphicPipeline* model_pipeline = Resources::createGraphicPipeline(pipeline_info, "MODEL_PIPELINE");
 
 		DefaultModelParserInfo parser_info{};
 		parser_info.graphic_pipeline = model_pipeline;
@@ -55,63 +57,37 @@ public:
 		model_info.flags = MODEL_FLAG_NONE;
 		model_info.parser = &parser;
 
-		Log::debug("Loading cube model");
-		model_info.path = "/models/cube/Cube.gltf";
-		//Model *cube_model = Resources::createModel(model_info, "cube");
-
 		Log::debug("Loading sponza model");
 		model_info.path = "/models/sponza/Sponza.gltf";
-		Model *sponza_model = Resources::createModel(model_info, "sponza");
+		Model* sponza_model = Resources::createModel(model_info, "sponza");
 
-		Log::debug("Loading teapot model");
-		model_info.path = "/models/teapot.gltf";
-		//Model* teapot_model = Resources::createModel(model_info, "teapot");
+		Log::debug("Loading beautiful-game model");
+		model_info.path = "/models/beautiful-game/ABeautifulGame.gltf";
+		Model* chess_model = Resources::createModel(model_info, "chess");
 
-		Log::debug("Loading dragon model");
-		model_info.path = "/models/dragon.gltf";
-		//Model* dragon_model = Resources::createModel(model_info, "dragon");
-
-		Log::debug("Loading avocado model");
-		model_info.path = "/models/avocado/Avocado.gltf";
-		//Model* avocado_model = Resources::createModel(model_info, "avocado");
 
 		//-------------------SCENE CREATION--------------------------------------
 
 		//Create camera
 		Entity camera_entity = createEntity3D();
-		Camera *camera = camera_entity.attach<Camera>();
-		CameraController *camera_controller = camera_entity.attach<CameraController>();
+		Camera* camera = camera_entity.attach<Camera>();
+		CameraController* camera_controller = camera_entity.attach<CameraController>();
 		camera_controller->invert_y = true;
 		camera_controller->speed = 10;
+		camera_entity.get<Transform>()->translate(vec3(0, 2, 0));
 		camera->setRenderTarget(Graphics::getDefaultRenderTarget());
 		setCameraEntity(camera_entity);
 
-		//auto teapot = createEntity3D();
-		//ModelRenderer *teapot_renderer = teapot.attach<ModelRenderer>();
-		//teapot.get<Transform>()->translate(vec3(0, 0, -5));
-		//teapot.get<Transform>()->setLocalScale(vec3(0.1));
-		//teapot_renderer->model = teapot_model;
+		auto chess = createEntity3D();
+		ModelRenderer* chess_renderer = chess.attach<ModelRenderer>();
+		chess_renderer->model = chess_model;
+		chess.get<Transform>()->setLocalScale(vec3(3));
+		chess.get<Transform>()->translate(vec3(0, 0, 0));
 
 		auto sponza = createEntity3D();
-		ModelRenderer *sponza_renderer = sponza.attach<ModelRenderer>();
+		ModelRenderer* sponza_renderer = sponza.attach<ModelRenderer>();
 		sponza_renderer->model = sponza_model;
 		sponza.get<Transform>()->setLocalScale(vec3(1));
-
-		//auto cube = createEntity3D();
-		//ModelRenderer *cube_renderer = cube.attach<ModelRenderer>();
-		//cube.get<Transform>()->translate(vec3(0, 0, -2));
-		//cube.get<Transform>()->setLocalScale(vec3(0.5));
-		//cube_renderer->model = cube_model;
-
-		//auto avocado = createEntity3D();
-		//ModelRenderer* avocado_renderer = avocado.attach<ModelRenderer>();
-		//avocado.get<Transform>()->translate(vec3(0, 0, -2));
-		//avocado.get<Transform>()->setLocalScale(vec3(50));
-		//avocado_renderer->model = avocado_model;
-
-		//auto dragon = createEntity3D();
-		//ModelRenderer *dragon_renderer = dragon.attach<ModelRenderer>();
-		//dragon.get<Transform>()->translate(vec3(0, 0, -10));
-		//dragon_renderer->model = dragon_model;
+		sponza.get<Transform>()->translate(vec3(0, 0, 0));
 	}
 };
